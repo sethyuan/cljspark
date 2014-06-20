1c85049c00e30d267f7ce685e48b7ebca06f7411
sethyuan <cavingdeep@gmail.com>
2014-06-20 17:16:43 +0800

    Fix some wrappings.

--- src/cav/cljspark/rdd.clj
+++ src/cav/cljspark/rdd.clj
@@ -2,13 +2,20 @@
   (:refer-clojure :exclude [map mapcat filter count reduce])
   (:require [clojure.core :as core])
   (:import [org.apache.spark.api.java JavaRDD]
-           [org.apache.spark.api.java.function Function Function2]))
+           [org.apache.spark.api.java.function
+            Function Function2 FlatMapFunction FlatMapFunction2]))
 
 (def ^:private func
   (memoize (fn [f] (reify Function (call [this x] (f x))))))
 
 (def ^:private func2
   (memoize (fn [f] (reify Function2 (call [this x y] (f x y))))))
+
+(def ^:private flat-func
+  (memoize (fn [f] (reify FlatMapFunction (call [this x] (f x))))))
+
+(def ^:private flat-func2
+  (memoize (fn [f] (reify FlatMapFunction2 (call [this x y] (f x y))))))
 
 (defn map
   "rdd.map"
@@ -18,12 +25,12 @@
 (defn mapcat
   "rdd.flatMap"
   [f ^JavaRDD rdd]
-  (.flatMap rdd (func f)))
+  (.flatMap rdd (flat-func f)))
 
 (defn filter
   "rdd.filter"
   [pred ^JavaRDD rdd]
-  (.filter rdd (func pred)))
+  (.filter rdd (func #(if (pred %) true false))))
 
 (defn count
   "rdd.count"
